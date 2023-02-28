e7dd3155ffe450a5e7b22b2497a490deedb2d15a
koyahee <40119630+koyahee@users.noreply.github.com>
2023-03-01 01:48:11 +0900

    manifest V2 → V3

--- src/background.js
+++ src/background.js
@@ -1,13 +1,3 @@
-// main.htmlを新しいタブで開く
-var showMainPage = function() {
-  chrome.windows.create({
-    url:'main.html',
-    type: 'popup',
-    width: 930, height: 500,
-        focused: true
-  });
-};
- 
-(function() {
-  chrome.browserAction.onClicked.addListener(showMainPage);
-}) ();
+chrome.action.onClicked.addListener((tab) => {
+  chrome.tabs.create({"url": "main.html"});
+});
